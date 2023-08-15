c5d2c8969a746c01399fdfc0d8d08f12ecae0132
jakChi <kobachincharauli8@gmail.com>
2023-08-15 19:37:45 +0400

    commited react files

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import './App.css';
+import { useState } from 'react';
+import MockData from './MOCK_DATA.json';
+import waves from './waves.svg';
+
+function App() {
+  const [ searchTerm, setSearchTerm ] = useState("");
+  const [ showBtn, setShowBtn ] = useState(false);
+
+  // const handleBtnShowing = () => {
+  //   if(searchTerm !== "") {
+  //     setShowBtn(true);
+  //   } else {
+  //     setShowBtn(false);
+  //   }
+  // }  
+
+  return (
+    <div className="App">
+      <h1 className='title'>Search Filter</h1>
+      <input 
+      id="search"
+      className='search-bar'
+      type='text' 
+      placeholder='what are u looking for?' 
+      required 
+      value={searchTerm} 
+      onChange={(e) => {
+        setSearchTerm(e.target.value); 
+        setShowBtn(true);
+      }}
+      />
+      <div className='search-items'>
+        {MockData.filter((person) => {
+        if(searchTerm === "") {
+          return null;
+        } else if (person.first_name.toLowerCase().includes(searchTerm.toLowerCase())) {
+          return person;
+        }
+        }).map((person, key) => {
+          return (
+            <div 
+            className='search-item' 
+            key={key}
+            >
+              {`${person.id}. ${person.first_name} ${person.last_name}`}
+            </div>
+          )
+        })}
+        {/*how to add scrolling from bottom to top?? this linking doesn't work*/}
+        {showBtn ? <button className='up-btn' href="#search">up</button> : null}
+      </div>
+    </div>
+  );
+}
+
+export default App;
